36cc4ed2aa5957a6f26015d4981fa1412f0ae325
Stas Alekseev <stas.alekseev@gmail.com>
2015-08-25 14:33:28 -0400

    various fixes

--- Puppetfile
+++ Puppetfile
@@ -2,6 +2,6 @@ forge 'https://forge.puppetlabs.com'
 
 mod 'puppetlabs/stdlib'
 mod 'puppetlabs/apt'
-
+mod 'puppetlabs/java'
 mod 'elasticsearch/elasticsearch',
   :git => 'https://github.com/elastic/puppet-elasticsearch'
--- site/profile/manifests/elasticsearch.pp
+++ site/profile/manifests/elasticsearch.pp
@@ -1,13 +1,10 @@
 class profile::elasticsearch ($config) {
-  class { '::elasticsearch':
-    manage_repo  => true,
-    repo_version => '1.7'
-  }
+  class { '::elasticsearch': }
   elasticsearch::instance { 'instance-01':
     config => $config,
   }
   elasticsearch::plugin { 'elasticsearch/elasticsearch-cloud-aws/2.7.0':
-    module_dir => 'elasticsearch-cloud-aws',
+    module_dir => 'cloud-aws',
     instances  => 'instance-01'
   }
 }
